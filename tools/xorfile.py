59caa391f418b88645006579a17f4904033fa420
Bu4275 <bu4275@gmail.com>
2024-01-13 13:40:07 +0800

    modify xorfile.py

--- tools/xorfile.py
+++ tools/xorfile.py
@@ -1,10 +1,9 @@
 import sys
 import os
 
-
 def xor(s, key):
     b = bytearray(len(s))
-    kb = key.encode('utf-8')
+    kb = key.encode('ascii')
     for i in range(len(s)):
         b[i] = (s[i] ^ kb[i % len(kb)]) & 0xff
     return bytes(b)
@@ -12,8 +11,15 @@ def xor(s, key):
 def caesar_encrypt(in_byte_array,s):
     return bytearray([(byte - s) & 0xFF for byte in in_byte_array])
 
-with open(sys.argv[1], 'rb') as f:
+filename = sys.argv[1]
+xor_key = sys.argv[2]
+output = 'decode_' + filename
+
+with open(filename, 'rb') as f:
     data = f.read()
-    de_data = caesar_encrypt(data, 11)
-    de_data = xor(de_data, 'adwocdmwa')
-    print(de_data.decode('utf-8'))
+    de_data = xor(data, xor_key)
+    with open(output, 'wb') as fw:
+        fw.write(de_data)
+
+print('Save to : %s' % output)
+
